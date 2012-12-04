e94b592b17697d7525acd81115656d6435ca27f4
Kary Betzabet <kary,bonilla@gmail.com>
2012-12-04 10:57:53 -0430

    creacion de metodos
    Clase cuenta bancaria

--- Practica/PracticaClasesGitHub/src/practicaclasesgithub/CuentaBancaria.java
+++ Practica/PracticaClasesGitHub/src/practicaclasesgithub/CuentaBancaria.java
@@ -3,7 +3,14 @@
  * and open the template in the editor.
  */
 package practicaclasesgithub;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import java.util.Date;
+import java.util.Calendar;
 
 /**
  *
@@ -13,9 +20,25 @@ import java.util.Date;
  * @author Naretza Ovalles
  */
 public class CuentaBancaria {
+
+    private String fechaApertura;
+    private long idCuentaBancaria;
+    private String numeroCuenta;
     
-    Date fechaApertura= new Date();
-            long idCuentaBancaria;
-            String numeroCuenta;
-    
+    ArrayList cliente = new ArrayList();
+
+    public CuentaBancaria() {
+    }
+
+    public CuentaBancaria(String fechaAper, long idCuenta, String numCuenta) {
+        fechaApertura = fechaAper;
+        idCuentaBancaria = idCuenta;
+        numeroCuenta = numCuenta;
+    }
+
+    public void createCuentaBancaria() {
+    }
+
+    public void getCuentaBancariaByID(long idCuenta) {
+    }
 }

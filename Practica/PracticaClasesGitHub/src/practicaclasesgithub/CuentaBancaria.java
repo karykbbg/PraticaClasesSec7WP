baece0cf2ac3262ecc1c41fb8d3a641883ab74a8
Kary Betzabet <kary,bonilla@gmail.com>
2012-12-04 10:08:04 -0430

    Creacion de la Practica
    * @author Kary Bonilla
    * @author Monica Batista
    * @author Johanna Duarte
    * @author Naretza Ovalles

---
+++ Practica/PracticaClasesGitHub/src/practicaclasesgithub/CuentaBancaria.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaclasesgithub;
+
+/**
+ *
+ * @author Kary Bonilla
+ * @author Monica Batista
+ * @author Johanna Duarte
+ * @author Naretza Ovalles
+ */
+public class CuentaBancaria {
+    
+}
---
+++ Practica/PracticaClasesGitHub/src/practicaclasesgithub/PracticaClasesGitHub.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaclasesgithub;
+
+/**
+ *
+ * @author Kary Bonilla
+ * @author Monica Batista
+ * @author Johanna Duarte
+ * @author Naretza Ovalles
+ */
+public class PracticaClasesGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}

b8bee9f6da368891ecbb74d4050dd5162d0b9af7
Johanna <duarterobles@gmail.com>
2012-12-04 19:11:39 -0430

    Creacion Clase Cliente y EntradaTeclado
    Se agrega la clase cliente, opcion inicial manejar los clientes a través
    de un listado estático.
    También incluyo  la clase de EntradaTeclado para agregar validaciones en
    la lectura por teclado

---
+++ Practica/PracticaClasesGitHub/src/practicaclasesgithub/Cliente.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaclasesgithub;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author unet
+ */
+public class Cliente {
+    private long idCliente;
+    private String nombreCliente;
+    private ArrayList<CuentaBancaria> misCuentasBancarias = new ArrayList<CuentaBancaria>();    
+    private static ArrayList<Cliente> listaClientes = new ArrayList<Cliente>();
+    
+    public void Cliente()
+    {
+        
+    }        
+    
+    public void createCliente() throws IOException
+    {
+      EntradaTeclado recibirDatosTeclado =  new EntradaTeclado();
+      System.out.println("Ingrese el identificador Unico del Cliente (debe ser un valor numérico) :\n" );
+      idCliente=recibirDatosTeclado.leerValorLong();      
+      System.out.println("Ingrese el nombre del Cliente:\n" );
+      nombreCliente=recibirDatosTeclado.leerCadenaCaracteres(100); 
+         
+      listaClientes.add(this);
+      
+      String opcionCrearCuenta;
+      do
+      {            
+            System.out.println("-> \n" );
+            System.out.println("Ingrese los datos de la cuenta bancaria :\n" );
+            CuentaBancaria CuentaBancariaInicial = new  CuentaBancaria();
+            CuentaBancariaInicial.createCuentaBancaria(); 
+            misCuentasBancarias.add(CuentaBancariaInicial);
+            
+            System.out.println("Desea agregar cuenta bancaria al Cliente ingrese (S) sí :\n" ); 
+               
+            opcionCrearCuenta =  recibirDatosTeclado.leerCadenaCaracteres(1); 
+            
+      }while( "S".equals(opcionCrearCuenta));
+      
+      
+    }
+}
---
+++ Practica/PracticaClasesGitHub/src/practicaclasesgithub/EntradaTeclado.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaclasesgithub;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/**
+ *
+ * @author unet
+ */
+public class EntradaTeclado {
+       
+        InputStreamReader lector;
+        BufferedReader entrada; 
+        
+        public void EntradaTeclado()
+        {
+        lector = new InputStreamReader(System.in);
+        entrada = new BufferedReader(lector);
+        }
+        
+        //método para la lectura por teclado de varibles long
+        public long leerValorLong() throws IOException
+        {
+            long valorTeclado=0;
+             boolean Errorlectura;         
+             do {
+                        try
+                        {
+                            Errorlectura=false;
+                            System.out.println("-> \n");  
+                            valorTeclado = new Long(Long.parseLong(entrada.readLine()));
+                        }catch (NumberFormatException error) 
+                        {                           
+                            System.out.println("Numero no válido\n" + error);  
+                            Errorlectura=true; 
+                            
+                        }
+             } while( Errorlectura == true);
+            
+            return valorTeclado;
+        }
+        
+        //método para leer por teclado una cadena de caracteres
+        public String leerCadenaCaracteres(int longitudMax) throws IOException
+        {
+             String valorTeclado="";
+             boolean ErrorValidacion;
+              do {
+                  ErrorValidacion = false;
+                   try
+                    {
+                        
+                     System.out.println("-> \n");
+                     valorTeclado = entrada.readLine();
+                     if( valorTeclado.length() > longitudMax )
+                     { 
+                        System.out.println("Tamaño no válido Máximo  \n" + longitudMax + " caracteres");  
+                     }
+                    }catch (IOException error) 
+                        {
+                           System.out.println("Error de lectura \n" + error);
+                           ErrorValidacion = true;                           
+                        }
+             } while( ErrorValidacion == true);
+             return valorTeclado;
+        }
+    
+}

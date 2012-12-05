5d5469ff364804f53196e45f3b12cfb52b7155f9
Monica Batista <moni_batista@yahoo.com>
2012-12-05 14:17:07 -0430

    Cambio de Clase Operacion Bancaria

--- Practica/PracticaClasesGitHub/src/practicaclasesgithub/OperacionBancaria.java
+++ Practica/PracticaClasesGitHub/src/practicaclasesgithub/OperacionBancaria.java
@@ -5,6 +5,7 @@
 package practicaclasesgithub;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Date;
@@ -19,7 +20,10 @@ public class OperacionBancaria {
     private Date fechaOperacion;
     private long idOperacion;
     private long montoOperacion;
-    ArrayList operacion = new ArrayList();
+    private long idTipocuenta;
+    private String numeroCuenta;
+    EntradaTeclado recibirDatosTeclado = new EntradaTeclado();
+    private ArrayList<OperacionBancaria> operacionesBancarias = new ArrayList<OperacionBancaria>();
 
     public OperacionBancaria() {
     }
@@ -27,12 +31,18 @@ public class OperacionBancaria {
     public OperacionBancaria(String numCuenta, long tipOp) {
     }
 
-    public void createOperacionBancaria() {
-        InputStreamReader entrada = new InputStreamReader(System.in);
-        BufferedReader lector = new BufferedReader(entrada);
-        System.out.println("Indique la Cédula del Cliente");
+    public void createOperacionBancaria() throws IOException {
+        System.out.println("Ingrese el id del tipo de cuenta: ");
+        idTipocuenta = recibirDatosTeclado.leerValorLong();
         System.out.println("Indique el tipo de operación a realizar: ");
+        numeroCuenta = recibirDatosTeclado.leerCadenaCaracteres(30);
+        System.out.println("El numero de Cuenta es: " + numeroCuenta);
+        java.util.Date fecha = new Date();
+        System.out.println(fecha);
+        System.out.println("se imprimio la fecha");
+        
     }
+
 
     public void getListOperacionesByDate(Date fechaIni, Date fechaFin) {
     }

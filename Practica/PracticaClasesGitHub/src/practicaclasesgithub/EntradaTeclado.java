957748138b6489ba8bad563b750309d9ee3539df
Johanna <duarterobles@gmail.com>
2012-12-04 20:39:42 -0430

    Cambios en la clase de EntradaTeclado

--- Practica/PracticaClasesGitHub/src/practicaclasesgithub/EntradaTeclado.java
+++ Practica/PracticaClasesGitHub/src/practicaclasesgithub/EntradaTeclado.java
@@ -7,87 +7,85 @@ package practicaclasesgithub;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 /**
  *
- * @author unet
+ * @author Kary Bonilla
+ * @author Monica Batista
+ * @author Johanna Duarte
+ * @author Naretza Ovalles
  */
 public class EntradaTeclado {
-       
-        InputStreamReader lector;
-        BufferedReader entrada; 
-        
-        public void EntradaTeclado()
-        {
-        lector = new InputStreamReader(System.in);
-        entrada = new BufferedReader(lector);
-        }
-        
-        public int leerValorEntero() throws IOException
-        {
-            int valorTeclado=0;
-            boolean Errorlectura; 
-             do{
-                    
-                    try
-                        {
-                         Errorlectura=false;
-                         System.out.println("-> \n");
-                         valorTeclado = Integer.parseInt(entrada.readLine());
-                        }catch (NumberFormatException error) 
-                        {                           
-                            System.out.println("Número no válido\n" + error);  
-                            Errorlectura=true;                             
-                        }                   
-                }while(Errorlectura == true);
-             
-            return valorTeclado;
-        }
-        
-        //método para la lectura por teclado de varibles long
-        public long leerValorLong() throws IOException
-        {
-            long valorTeclado=0;
-             boolean Errorlectura;         
-             do {
-                        try
-                        {
-                            Errorlectura=false;
-                            System.out.println("-> \n");  
-                            valorTeclado = Long.parseLong(entrada.readLine());
-                        }catch (IOException error) 
-                        {                           
-                            System.out.println("Numero no válido\n" + error);  
-                            Errorlectura=true; 
-                            
-                        }
-             } while( Errorlectura == true);
-            
-            return valorTeclado;
-        }
-        
-        //método para leer por teclado una cadena de caracteres
-        public String leerCadenaCaracteres(int longitudMax) throws IOException
-        {
-             String valorTeclado="";
-             boolean ErrorValidacion;
-              do {
-                  ErrorValidacion = false;
-                   try
-                    {
-                        
-                     System.out.println("-> \n");
-                     valorTeclado = entrada.readLine();
-                     if( valorTeclado.length() > longitudMax )
-                     { 
-                        System.out.println("Tamaño no válido Máximo  \n" + longitudMax + " caracteres");  
-                     }
-                    }catch (IOException error) 
-                        {
-                           System.out.println("Error de lectura \n" + error);
-                           ErrorValidacion = true;                           
-                        }
-             } while( ErrorValidacion == true);
-             return valorTeclado;
-        } 
-    
+
+    public void EntradaTeclado() {
+    }
+
+    public int leerValorEntero() throws IOException {
+
+        InputStreamReader lector = new InputStreamReader(System.in);
+        BufferedReader entrada = new BufferedReader(lector);
+
+        int valorTeclado = 0;
+        boolean Errorlectura;
+        do {
+
+            try {
+                Errorlectura = false;
+                System.out.println("-> \n");
+                valorTeclado = Integer.parseInt(entrada.readLine());
+            } catch (NumberFormatException error) {
+                System.out.println("Número no válido\n" + error);
+                Errorlectura = true;
+            }
+        } while (Errorlectura == true);
+
+        return valorTeclado;
+    }
+
+    //método para la lectura por teclado de varibles long
+    public long leerValorLong() throws IOException {
+        InputStreamReader lector = new InputStreamReader(System.in);
+        BufferedReader entrada = new BufferedReader(lector);
+
+        long valorTeclado = 0;
+        boolean Errorlectura;
+        do {
+            try {
+                Errorlectura = false;
+                System.out.println("-> \n");
+                valorTeclado = Long.parseLong(entrada.readLine());
+            } catch (IOException error) {
+                System.out.println("Numero no válido\n" + error);
+                Errorlectura = true;
+
+            }
+        } while (Errorlectura == true);
+
+        return valorTeclado;
+    }
+
+    //método para leer por teclado una cadena de caracteres
+    public String leerCadenaCaracteres(int longitudMax) throws IOException {
+
+        InputStreamReader lector = new InputStreamReader(System.in);
+        BufferedReader entrada = new BufferedReader(lector);
+
+        String valorTeclado = "";
+        boolean ErrorValidacion;
+        do {
+            ErrorValidacion = false;
+            try {
+
+                System.out.println("-> \n");
+                valorTeclado = entrada.readLine();
+                if (valorTeclado.length() > longitudMax) {
+                    System.out.println("Tamaño no válido Máximo  \n" + longitudMax + " caracteres");
+                }
+            } catch (IOException error) {
+                System.out.println("Error de lectura \n" + error);
+                ErrorValidacion = true;
+            }
+        } while (ErrorValidacion == true);
+        return valorTeclado;
+    }
 }

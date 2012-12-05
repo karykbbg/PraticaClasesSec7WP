d41db83ba01b53596af6328c57367561f2a05cf2
Naretza Ovalles <nayuovga@gmail.com>
2012-12-05 08:17:11 -0430

    Comentado algunos paquetes

--- Practica/PracticaClasesGitHub/src/practicaclasesgithub/TipoCuenta.java
+++ Practica/PracticaClasesGitHub/src/practicaclasesgithub/TipoCuenta.java
@@ -4,9 +4,9 @@
  */
 package practicaclasesgithub;
 
-import java.io.BufferedReader;
+//import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+//import java.io.InputStreamReader;
 import java.util.*;
 
 /**
